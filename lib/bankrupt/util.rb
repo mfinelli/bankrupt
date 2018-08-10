db07f065a7a7b766236721a5988662b9297c37ba
Mario Finelli <mario@finel.li>
2018-08-10 11:05:33 +0200

    Add missing documentation

--- lib/bankrupt/util.rb
+++ lib/bankrupt/util.rb
@@ -3,6 +3,10 @@
 module Bankrupt
   # Utilities for working with asset manifest files.
   class Util
+    # Parse the asset manifest
+    #
+    # @param path [String] path to the manifest
+    # @return [Hash] parsed manifest or empty hash on error
     def self.parse_manifest(path)
       Hash[JSON.parse(File.read(path)).map do |k, v|
         [k.freeze, Hash[v.map { |l, b| [l.to_sym, b.freeze] }].freeze]
